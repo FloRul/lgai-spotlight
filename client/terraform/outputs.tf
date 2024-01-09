3627095f080ba15ca719071b90ed3fe066d7effb
FloRul <rumiel.florian@gmail.com>
2024-01-09 09:04:53 -0500

    Update S3 bucket website configuration

--- client/terraform/outputs.tf
+++ client/terraform/outputs.tf
@@ -1,6 +1,6 @@
-# output "s3_bucket_website_url" {
-#   value = aws_s3_bucket_website_configuration.website_config.website_endpoint
-# }
+output "s3_bucket_website_url" {
+  value = aws_s3_bucket_website_configuration.website_config.website_endpoint
+}
 
 output "deployment_bucket" {
   value = aws_s3_bucket.client_deployment_bucket.id
--- terraform/ouptputs.tf
+++ terraform/ouptputs.tf
@@ -7,9 +7,9 @@ output "deployment_bucket" {
   value = module.client_website_deployment_bucket.deployment_bucket
 }
 
-# output "s3_bucket_website_url" {
-#   value = "http://${module.client_website_deployment_bucket.s3_bucket_website_url}/deployment/index.html"
-# }
+output "s3_bucket_website_url" {
+  value = "http://${module.client_website_deployment_bucket.s3_bucket_website_url}/deployment/index.html"
+}
 
 output "aws_region" {
   value = var.aws_region
